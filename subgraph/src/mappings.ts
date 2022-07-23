868c7db4b142429f4ffd443e557eb0de3a677296
Simon Emanuel Schmid <simon@edgeandnode.com>
2022-07-24 00:44:21 +0200

    Init subgraph

---
+++ subgraph/src/mappings.ts
@@ -0,0 +1,65 @@
+import { PoolCreated as PoolCreatedEvent } from "../generated/AdamfoProtocol/AdamfoProtocol";
+import {
+  RegisterExpense as RegisterExpenseEvent,
+  TransferBatch as TransferBatchEvent,
+  TransferSingle as TransferSingleEvent,
+  URI as URIEvent
+} from "../generated/templates/AdamfoPool/AdamfoPool";
+import {
+  RegisterExpense,
+  TransferBatch,
+  TransferSingle,
+  URI,
+  PoolCreated
+} from "../generated/schema";
+
+export function handlePoolCreated(event: PoolCreatedEvent): void {
+  let entity = new PoolCreated(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  );
+  entity.childAddress = event.params.childAddress;
+  entity.members = event.params.members;
+  entity.save();
+}
+
+export function handleRegisterExpense(event: RegisterExpenseEvent): void {
+  let entity = new RegisterExpense(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  );
+  entity.lender = event.params.lender;
+  entity.participants = event.params.participants;
+  entity.amount = event.params.amount;
+  entity.description = event.params.description;
+  entity.save();
+}
+
+export function handleTransferBatch(event: TransferBatchEvent): void {
+  let entity = new TransferBatch(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  );
+  entity.operator = event.params.operator;
+  entity.from = event.params.from;
+  entity.to = event.params.to;
+  entity.ids = event.params.ids;
+  entity.values = event.params.values;
+  entity.save();
+}
+
+export function handleTransferSingle(event: TransferSingleEvent): void {
+  let entity = new TransferSingle(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  );
+  entity.operator = event.params.operator;
+  entity.from = event.params.from;
+  entity.to = event.params.to;
+  entity.value = event.params.value;
+  entity.save();
+}
+
+export function handleURI(event: URIEvent): void {
+  let entity = new URI(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  );
+  entity.value = event.params.value;
+  entity.save();
+}
