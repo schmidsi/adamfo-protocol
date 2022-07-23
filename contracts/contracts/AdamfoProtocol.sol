2467dd010a9ee7402b273c153f26bb82a7e0c466
Simon Emanuel Schmid <simon@edgeandnode.com>
2022-07-23 16:53:54 +0200

    Credit is removed if new dept accrues

--- contracts/contracts/AdamfoProtocol.sol
+++ contracts/contracts/AdamfoProtocol.sol
@@ -7,17 +7,70 @@ import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "hardhat/console.sol";
 
 contract AdamfoProtocol is ERC1155 {
-    uint256 public constant GOLD = 0;
-    uint256 public constant SILVER = 1;
-    uint256 public constant THORS_HAMMER = 2;
-    uint256 public constant SWORD = 3;
-    uint256 public constant SHIELD = 4;
+    uint256 public constant MEMBER_TOKEN = 0;
+    uint256 public constant CREDIT = 1;
 
-    constructor() ERC1155("https://game.example/api/item/{id}.json") {
-        _mint(msg.sender, GOLD, 10**18, "");
-        _mint(msg.sender, SILVER, 10**27, "");
-        _mint(msg.sender, THORS_HAMMER, 1, "");
-        _mint(msg.sender, SWORD, 10**9, "");
-        _mint(msg.sender, SHIELD, 10**9, "");
+    mapping(address => uint256) public dept;
+
+    event RegisterExpense(
+        address lender,
+        address[] participants,
+        uint256 amount,
+        string description
+    );
+
+    // The rest are NFTs
+
+    constructor(address[] memory members)
+        ERC1155("https://adamfo.xyz/api/item/{id}.json")
+    {
+        for (uint i = 0; i < members.length; i++) {
+            _mint(members[i], MEMBER_TOKEN, 1, "");
+        }
+    }
+
+    function registerExpense(
+        address payer,
+        address[] memory participants,
+        uint256 amount,
+        string memory description
+    ) public {
+        require(balanceOf(payer, MEMBER_TOKEN) >= 1, "Payer must be member");
+        require(participants.length >= 1, "Participants must be at least one");
+
+        // TODO: Check if this is an unsafe math operation
+        uint256 deptPerBorrower = amount / participants.length;
+        uint256 remainingDept = 0;
+        uint256 creditOfParticipant = 0;
+
+        for (uint i = 0; i < participants.length; i++) {
+            require(
+                balanceOf(participants[i], MEMBER_TOKEN) >= 1,
+                "Participant must be member"
+            );
+
+            if (participants[i] != payer) {
+                creditOfParticipant = balanceOf(participants[i], CREDIT);
+
+                if (deptPerBorrower >= creditOfParticipant) {
+                    remainingDept = deptPerBorrower - creditOfParticipant;
+                    _burn(participants[i], CREDIT, creditOfParticipant);
+                    _mint(payer, CREDIT, remainingDept, "");
+                    dept[participants[i]] =
+                        dept[participants[i]] +
+                        remainingDept;
+                } else {
+                    _burn(participants[i], CREDIT, deptPerBorrower);
+                }
+            }
+        }
+
+        emit RegisterExpense(payer, participants, amount, description);
+    }
+
+    function payBack(address member) public payable {
+        require(balanceOf(member, MEMBER_TOKEN) >= 1, "Must be (for) a member");
+        require(msg.value > 0, "Must pay back debt");
+        dept[member] = dept[member] - msg.value;
     }
 }
--- contracts/test/AdamfoProtocol.ts
+++ contracts/test/AdamfoProtocol.ts
@@ -1,12 +1,40 @@
 import { ethers } from "hardhat";
+import { expect } from "chai";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { AdamfoProtocol } from "../typechain-types";
 
 describe("AdamfoProtocol", function () {
-  it("Should deploy", async () => {
-    const [owner, other] = await ethers.getSigners();
+  let alice: SignerWithAddress,
+    bob: SignerWithAddress,
+    charly: SignerWithAddress;
+  let protocol: AdamfoProtocol;
+
+  it("Deploy", async () => {
+    [alice, bob, charly] = await ethers.getSigners();
 
     const AdamfoProtocol = await ethers.getContractFactory("AdamfoProtocol");
-    const protocol = await AdamfoProtocol.deploy();
+    protocol = await AdamfoProtocol.deploy([
+      alice.address,
+      bob.address,
+      charly.address,
+    ]);
+
+    expect(await protocol.balanceOf(alice.address, 0)).to.equal("1");
+    expect(await protocol.balanceOf(bob.address, 0)).to.equal("1");
+    expect(await protocol.balanceOf(charly.address, 0)).to.equal("1");
 
     // return { protocol, owner, other };
+  });
+
+  it("Register dept", async () => {
+    await protocol.registerExpense(
+      alice.address,
+      [alice.address, bob.address, charly.address],
+      9,
+      "Dinner"
+    );
+    expect(await protocol.balanceOf(alice.address, 1)).to.equal("6");
+    // expect(await protocol.balanceOf(alice.address, 1)).to.equal("6");
+    // expect(await protocol.balanceOf(alice.address, 1)).to.equal("6");
   });
 });
